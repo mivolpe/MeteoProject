c9ac586c86839db94f0bba7716a1b0cd05f3d076
disiane <bettega1365@hotmail.com>
2021-03-24 09:11:51 +0100

    feat : show complete trame in a grid

--- ProjetMeteo/WindowsFormsApp5/Form1.Designer.cs
+++ ProjetMeteo/WindowsFormsApp5/Form1.Designer.cs
@@ -28,7 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.grid = new System.Windows.Forms.DataGridView();
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.btLeave = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,14 +52,57 @@
             this.grid.Size = new System.Drawing.Size(429, 150);
             this.grid.TabIndex = 0;
             // 
+            // port
+            // 
+            this.port.PortName = "COM2";
+            this.port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.port_DataReceived);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(669, 48);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(119, 65);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(669, 133);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(119, 65);
+            this.btStop.TabIndex = 2;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // btLeave
+            // 
+            this.btLeave.Location = new System.Drawing.Point(669, 373);
+            this.btLeave.Name = "btLeave";
+            this.btLeave.Size = new System.Drawing.Size(119, 65);
+            this.btLeave.TabIndex = 3;
+            this.btLeave.Text = "Leave";
+            this.btLeave.UseVisualStyleBackColor = true;
+            this.btLeave.Click += new System.EventHandler(this.btLeave_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btLeave);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
             this.Controls.Add(this.grid);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
             this.ResumeLayout(false);
 
@@ -62,6 +111,11 @@
         #endregion
 
         private System.Windows.Forms.DataGridView grid;
+        private System.IO.Ports.SerialPort port;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btLeave;
     }
 }
 
--- ProjetMeteo/WindowsFormsApp5/Form1.cs
+++ ProjetMeteo/WindowsFormsApp5/Form1.cs
@@ -7,14 +7,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace WindowsFormsApp5
 {
     public partial class Form1 : Form
     {
+        DataTable dt = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btLeave_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            btStart.Enabled = false;
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            port.Open();
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            btStart.Enabled = true;
+            port.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //transfertInList();
+        }
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] data = new byte[port.BytesToRead];
+            port.Read(data, 0, data.Length);
+            transfertInList(data);
+        }
+
+        private bool trameComplete (byte[] data)
+        {
+            //fonction qui verifie si une trame est complète
+            bool rep = false;
+            int nbreData = 0;
+
+            if (data.Length >= 10) //  taille minium d'une trame
+            {
+                if (data[0] == 85 && data[1] == 170 && data[2] == 85) //verifie si il y a le début de liste
+                {
+                    nbreData = data[4];
+                    if (data[7+nbreData] == 170 && data[8 + nbreData] == 85 && data[9 + nbreData] == 170)//verifie si il y a la fin de liste
+                    {
+                        rep = true;
+                    }
+                }
+            }
+            return rep;
+        }
+
+        private void transfertInList(byte[] buffer)
+        {
+            int id = 0;
+            int nbreData = 0;
+            int type = 0;
+            UInt32 data = 0;
+            int checkSum = 0;
+
+            if (trameComplete(buffer))
+            {
+                id = buffer[3];
+                nbreData = buffer[4];
+                type = buffer[5];
+                for(int i = 0; i < nbreData; i++)
+                {
+                    data += (UInt32)(buffer[6 + i]) << i * 8;
+                }
+                checkSum = buffer[6+nbreData];
+                dataInGrid(id, nbreData, type, (int)data, checkSum);
+                grid.DataSource = dt;
+            }
+        }
+
+        private void createGrid ()
+        {
+            dt.Columns.Add("Id", typeof(int));
+            dt.Columns.Add("Nbre de Data", typeof(int));
+            dt.Columns.Add("Type", typeof(int));
+            dt.Columns.Add("Data", typeof(int));
+            dt.Columns.Add("Checksum", typeof(int));
+
+            grid.DataSource = dt;
+            grid.Visible = true;
+        }
+
+        private void dataInGrid (int id, int nbreData, int type, int data, int checkSum)
+        {
+            dt.Rows.Add(new Object[] { id, nbreData, type, data, checkSum });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createGrid();
         }
     }
 }
