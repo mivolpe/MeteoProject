61f72d457e2e5acb98b43d97eb3566396eb2f712
mivolpe <michele.volpe@helb-prigogine.be>
2021-06-02 16:52:04 +0200

    refactor : delete creation table

--- ProjetMeteo/WindowsFormsApp5/Form1.cs
+++ ProjetMeteo/WindowsFormsApp5/Form1.cs
@@ -19,9 +19,6 @@ namespace WindowsFormsApp5
     public partial class Form1 : Form
     {
         RequestDB requestDB = new RequestDB();
-        DataSet UserAccount = new DataSet();
-        internal static DataTable Local_UserTable = new DataTable();
-        internal static DataTable Local_AccessTable = new DataTable();
         DataTable dt = new DataTable();
         public static String username = "pas connecté";
         public static int accessLvl = 5;
@@ -30,25 +27,6 @@ namespace WindowsFormsApp5
         public Form1()
         {
             InitializeComponent();
-            configDataset();
-            createTable();
-        }
-
-        private void createTable()
-        {
-            // Creating OLEDB connection string for Ms-Access 2007 database file
-
-            OleDbConnection myConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;"
-                +
-                @"Data Source=..\..\DB\DB_UserAccess1.accdb;Cache Authentication=True");
-            myConnection.Open();
-            // Create Oledb command to execute particular query
-            OleDbCommand myCommand = new OleDbCommand();
-            myCommand.Connection = myConnection;
-            // Query to create table with specified data columne
-            myCommand.CommandText = "CREATE TABLE tblIdentityTesting([MyIdentityColumn] long, [Name] text)";
-            myCommand.ExecuteNonQuery();
-            MessageBox.Show("Table Created Successfully");
         }
 
         private void btLeave_Click(object sender, EventArgs e)
@@ -134,95 +112,6 @@ namespace WindowsFormsApp5
         {
             labUsername.Text = username;
             labAccess.Text = "Niveau d'acces: "+accessLvl;
-        }
-
-        private void configDataset()
-        {
-            DataColumn UserKey_ID = new DataColumn("ID", System.Type.GetType("System.Int16"));
-            DataColumn UserName = new DataColumn("UserName", System.Type.GetType("System.String"));
-            DataColumn UserPassword = new DataColumn("Password", System.Type.GetType("System.String"));
-            DataColumn Access_Id = new DataColumn("Access_ID", System.Type.GetType("System.Int16"));
-
-            DataColumn AccessKey_Id = new DataColumn("E", System.Type.GetType("System.Int16"));
-            DataColumn AccessName = new DataColumn("F", System.Type.GetType("System.String"));
-            DataColumn AllowCreateID = new DataColumn("G", System.Type.GetType("System.Boolean"));
-            DataColumn AllowDestroyID = new DataColumn("H", System.Type.GetType("System.Boolean"));
-            DataColumn AllowConfigAlarm = new DataColumn("I", System.Type.GetType("System.Boolean"));
-            DataColumn UserCreation = new DataColumn("J", System.Type.GetType("System.Boolean"));
-
-            Local_UserTable.TableName = "Local_UserTable";
-            Local_AccessTable.TableName = "Local_AccessTable";
-
-            UserAccount.Tables.Add(Local_UserTable);
-            UserAccount.Tables.Add(Local_AccessTable);
-
-            UserKey_ID.AutoIncrement = true;
-            UserKey_ID.Unique = true;
-            UserKey_ID.ColumnName = "UserKey_ID";
-            UserKey_ID.DataType = System.Type.GetType("System.Int32");
-            Local_UserTable.Columns.Add(UserKey_ID);
-
-            UserName.AutoIncrement = false;
-            UserName.Unique = false;
-            UserName.ColumnName = "UserName";
-            UserName.DataType = System.Type.GetType("System.String");
-            Local_UserTable.Columns.Add(UserName);
-
-            UserPassword.AutoIncrement = false;
-            UserPassword.Unique = false;
-            UserPassword.ColumnName = "UserPassword";
-            UserPassword.DataType = System.Type.GetType("System.String");
-            Local_UserTable.Columns.Add(UserPassword);
-
-            Access_Id.AutoIncrement = false;
-            Access_Id.Unique = false;
-            Access_Id.ColumnName = "Access_Id";
-            Access_Id.DataType = System.Type.GetType("System.Int32");
-            Local_UserTable.Columns.Add(Access_Id);
-
-            AccessKey_Id.AutoIncrement = true;
-            AccessKey_Id.Unique = true;
-            AccessKey_Id.ColumnName = "AccessKey_Id";
-            AccessKey_Id.DataType = System.Type.GetType("System.Int32");
-            Local_AccessTable.Columns.Add(AccessKey_Id);
-
-            AccessName.AutoIncrement = false;
-            AccessName.Unique = false;
-            AccessName.ColumnName = "AccessName";
-            AccessName.DataType = System.Type.GetType("System.String");
-            Local_AccessTable.Columns.Add(AccessName);
-
-            AllowCreateID.AutoIncrement = false;
-            AllowCreateID.Unique = false;
-            AllowCreateID.ColumnName = "AllowCreateID";
-            AllowCreateID.DataType = System.Type.GetType("System.Boolean");
-            Local_AccessTable.Columns.Add(AllowCreateID);
-
-            AllowDestroyID.AutoIncrement = false;
-            AllowDestroyID.Unique = false;
-            AllowDestroyID.ColumnName = "AllowDestroyID";
-            AllowDestroyID.DataType = System.Type.GetType("System.Boolean");
-            Local_AccessTable.Columns.Add(AllowDestroyID);
-
-            AllowConfigAlarm.AutoIncrement = false;
-            AllowConfigAlarm.Unique = false;
-            AllowConfigAlarm.ColumnName = "AllowConfigAlarm";
-            AllowConfigAlarm.DataType = System.Type.GetType("System.Boolean");
-            Local_AccessTable.Columns.Add(AllowConfigAlarm);
-
-            UserCreation.AutoIncrement = false;
-            UserCreation.Unique = false;
-            UserCreation.ColumnName = "UserCreation";
-            UserCreation.DataType = System.Type.GetType("System.Boolean");
-            Local_AccessTable.Columns.Add(UserCreation);
-
-            DataColumn parentColumn = UserAccount.Tables["Local_AccessTable"].Columns["AccessKey_Id"];
-            DataColumn childColumn = UserAccount.Tables["Local_UserTable"].Columns["Access_Id"];
-
-            DataRelation relation = new DataRelation("parent2Child", parentColumn, childColumn);
-
-            UserAccount.Tables["Local_UserTable"].ParentRelations.Add(relation);
-
         }
     }
 }
